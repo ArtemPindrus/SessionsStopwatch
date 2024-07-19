3e1192539c468fc47d5f62395d84104e4ab8b311
Artem Pindrus <artempindrus@gmail.com>
2024-07-19 13:53:55 +0300

    Rename and doc

---
+++ SessionsStopwatch/Commands/ChangeWindowVisibilityCommand.cs
@@ -0,0 +1,18 @@
+﻿using StoreApp.Commands;
+using System.Windows;
+
+namespace SessionsStopwatch.Commands {
+    /// <summary>
+    /// Command used to switch <see cref="Window"/> visibility.
+    /// </summary>
+    /// <param name="window">Associated <see cref="Window"/>.</param>
+    public class ChangeWindowVisibilityCommand(Window window) : CommandBase {
+        private readonly Window window = window;
+
+        /// <inheritdoc/>
+        public override void Execute(object? parameter) {
+            if (window.IsVisible) window.Hide();
+            else window.Show();
+        }
+    }
+}
