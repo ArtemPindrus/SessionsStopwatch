96cd6d7c9ce5a892793d7a8968b4104e7f580ef9
Artem Pindrus <artempindrus@gmail.com>
2024-07-19 14:00:39 +0300

    Docs

--- SessionsStopwatch/Commands/DeleteReminderCommand.cs
+++ SessionsStopwatch/Commands/DeleteReminderCommand.cs
@@ -1,21 +1,38 @@
 ﻿using SessionsStopwatch.Utilities;
 using SessionsStopwatch.ViewModels;
+using SessionsStopwatch.Views;
 using StoreApp.Commands;
+using System.ComponentModel;
 
-namespace SessionsStopwatch.Commands
-{
-    class DeleteReminderCommand : CommandBase {
-        private readonly AppSettingsVM _vm;
+namespace SessionsStopwatch.Commands {
+    /// <summary>
+    /// Command allows deletion of reminders from datagrid of <see cref="AppSettingsView"/>.
+    /// </summary>
+    public class DeleteReminderCommand : CommandBase {
+        private readonly AppSettingsVM vm;
 
-        public DeleteReminderCommand(AppSettingsVM vm) { 
-            _vm = vm;
-            vm.LastSelectedChanged += OnCanExecuteChanged;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DeleteReminderCommand"/> class associated with a <see cref="AppSettingsVM"/>.
+        /// </summary>
+        /// <param name="vm">Instance of associated <see cref="AppSettingsVM"/>.</param>
+        public DeleteReminderCommand(AppSettingsVM vm) {
+            this.vm = vm;
+            vm.PropertyChanged += HandlePropertyChanged;
         }
 
+        /// <summary>
+        /// Deletes <see cref="AppSettingsVM.LastSelected"/> reminder of <see cref="vm"/> from the <see cref="AppStopwatch"/>.
+        /// </summary>
+        /// <param name="parameter"><inheritdoc/></param>
         public override void Execute(object? parameter) {
-            if (_vm.LastSelected != null) AppStopwatch.DeleteReminder(_vm.LastSelected);
+            if (vm.LastSelected != null) AppStopwatch.DeleteReminder(vm.LastSelected);
         }
 
-        public override bool CanExecute(object? parameter) => _vm.LastSelected != null;
+        /// <inheritdoc/>
+        public override bool CanExecute(object? parameter) => vm.LastSelected != null;
+
+        private void HandlePropertyChanged(object? sender, PropertyChangedEventArgs e) {
+            if (e.PropertyName == nameof(vm.LastSelected)) OnCanExecuteChanged();
+        }
     }
 }
--- SessionsStopwatch/Commands/OpenAddReminderWindow.cs
+++ SessionsStopwatch/Commands/OpenAddReminderWindow.cs
@@ -2,12 +2,18 @@
 using SessionsStopwatch.Windows;
 using StoreApp.Commands;
 
-namespace SessionsStopwatch.Commands
-{
-    class OpenAddReminderWindow : CommandBase {
+namespace SessionsStopwatch.Commands {
+    /// <summary>
+    /// Opens <see cref="AddReminderWindow"/>, creating a new instance of it.
+    /// </summary>
+    internal class OpenAddReminderWindow : CommandBase {
+        /// <summary>
+        /// Opens new <see cref="AddReminderWindow"/>.
+        /// </summary>
+        /// <param name="parameter"><inheritdoc/></param>
         public override void Execute(object? parameter) {
-            AddAReminderWindow window = new();
-            AddAReminderVM vm = new(window);
+            AddReminderWindow window = new();
+            AddReminderVM vm = new(window);
             window.DataContext = vm;
             window.Show();
         }
