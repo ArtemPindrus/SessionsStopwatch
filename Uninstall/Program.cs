d04243dae29e246e5b609fe441e2224809978cf3
Artem Pindrus <artempindrus@gmail.com>
2024-07-19 11:26:56 +0300

    Uninstalling

---
+++ Uninstall/Program.cs
@@ -0,0 +1,7 @@
+﻿using SessionsStopwatch.Utilities;
+
+TaskSchedulerStartTask.UnregisterTask();
+Console.WriteLine($"Task unregistered. \nNow you can delete the folder with application.");
+
+
+Console.ReadLine();
