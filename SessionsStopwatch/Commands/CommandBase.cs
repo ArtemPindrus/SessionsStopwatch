1082e495ab8561402d323d1b79ee1a132c6c94bc
Artem Pindrus <artempindrus@gmail.com>
2024-07-19 13:50:39 +0300

    Doc

--- SessionsStopwatch/Commands/CommandBase.cs
+++ SessionsStopwatch/Commands/CommandBase.cs
@@ -1,13 +1,22 @@
 ﻿using System.Windows.Input;
 
-namespace StoreApp.Commands
-{
+namespace StoreApp.Commands {
+    /// <summary>
+    /// Defines base class for all Commands to inherit.
+    /// </summary>
     public abstract class CommandBase : ICommand {
+        /// <inheritdoc/>
         public event EventHandler? CanExecuteChanged;
 
+        /// <inheritdoc/>
         public virtual bool CanExecute(object? parameter) => true;
+
+        /// <inheritdoc/>
         public abstract void Execute(object? parameter);
 
+        /// <summary>
+        /// Provides a concise way to invoke <see cref="CanExecuteChanged"/> event.
+        /// </summary>
         protected void OnCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
     }
 }
