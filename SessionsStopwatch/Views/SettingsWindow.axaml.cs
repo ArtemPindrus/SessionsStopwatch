e13e8d3886c3eef1464717ec8d2db39135902337
Artem Pindrus <artempindrus@gmail.com>
2024-11-27 22:26:48 +0200

    Populate ComboBox

--- SessionsStopwatch/Views/SettingsWindow.axaml.cs
+++ SessionsStopwatch/Views/SettingsWindow.axaml.cs
@@ -9,5 +9,14 @@ public partial class SettingsWindow : Window {
     public SettingsWindow() {
         InitializeComponent();
         this.RegisterDragWindow(this);
+
+        Type baseType = typeof(Reminder);
+
+        var items = Assembly.GetExecutingAssembly()
+            .GetTypes()
+            .Where(x => x != baseType && x.IsAssignableTo(typeof(Reminder)))
+            .ToArray();
+        AddReminderTypeSelector.ItemsSource = items;
+    }
     }
 }
