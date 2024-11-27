d4a8a5202e0f6bdb7c2c4c95c185e7ff2efb91c6
Artem Pindrus <artempindrus@gmail.com>
2024-11-27 17:20:52 +0200

    Update Program.cs

--- RestartStopwatchTask/Program.cs
+++ RestartStopwatchTask/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System.IO.Pipes;
+using System.Text;
 
 using var client = new NamedPipeClientStream(".", "RestartPipe", PipeDirection.InOut);
 client.Connect();
 
-using var writer = new StreamWriter(client);
+using var writer = new StreamWriter(client, Encoding.UTF8, -1, true);
 writer.WriteLine("RestartStopwatch");
