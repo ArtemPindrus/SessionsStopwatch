5b34f73039e1dc6c0efa14ec86260253358063dc
Artem Pindrus <artempindrus@gmail.com>
2024-11-27 22:27:13 +0200

    RemindWindow

---
+++ SessionsStopwatch/ViewModels/RemindWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using SessionsStopwatch.Utilities;
+using SessionsStopwatch.Views;
+
+namespace SessionsStopwatch.ViewModels;
+
+public partial class RemindWindowViewModel : ViewModelBase {
+    public string ReminderText { get; }
+    
+    public RemindWindowViewModel(string reminderText) {
+        ReminderText = reminderText;
+    }
+    
+    [RelayCommand]
+    private void Close() {
+        WindowUtility.CloseFirst<RemindWindow>();
+    }
+}
---
+++ SessionsStopwatch/Views/RemindWindow.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using SessionsStopwatch.Utilities;
+
+namespace SessionsStopwatch.Views;
+
+public partial class RemindWindow : Window {
+    public RemindWindow() {
+        InitializeComponent();
+        this.RegisterDragWindow(this);
+    }
+}
