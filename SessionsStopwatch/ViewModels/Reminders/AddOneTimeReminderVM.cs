5c6dc5eed71bdfd5c3e24b21bb1cefc212c32c64
Artem Pindrus <artempindrus@gmail.com>
2024-11-27 22:25:40 +0200

    AddOneTimeReminder view

---
+++ SessionsStopwatch/ViewModels/Reminders/AddOneTimeReminderVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using SessionsStopwatch.Models.Reminding;
+
+namespace SessionsStopwatch.ViewModels.Reminders;
+
+public partial class AddOneTimeReminderVM : ViewModelBase {
+    private TimeSpan lastParsedTime;
+    
+    [NotifyCanExecuteChangedFor(nameof(AddCommand))]
+    [ObservableProperty] 
+    private string? timeTextBox;
+    
+    [RelayCommand(CanExecute = nameof(CanAdd))]
+    private void Add() {
+        OneTimeReminder reminder = new(lastParsedTime);
+        
+        App.RemindersManager.AddReminder(reminder);
+    }
+
+    private bool CanAdd() {
+        return TimeSpan.TryParse(TimeTextBox, out lastParsedTime);
+    }
+}
---
+++ SessionsStopwatch/Views/Reminders/AddOneTimeReminder.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SessionsStopwatch.Views.Reminders;
+
+public partial class AddOneTimeReminder : UserControl {
+    public AddOneTimeReminder() {
+        InitializeComponent();
+    }
+}
