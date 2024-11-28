31e0cc25882f61aeb57f5c27e6409485fe2ba8e4
Artem Pindrus <artempindrus@gmail.com>
2024-11-28 20:03:06 +0200

    Base for AddReminder ViewModels

---
+++ SessionsStopwatch/ViewModels/Reminders/AddReminderBaseVM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SessionsStopwatch.ViewModels.Reminders;
+
+public abstract class AddReminderBaseVM : ViewModelBase {
+    public event Action? AddedReminder;
+
+    protected void OnAddedReminder() {
+        AddedReminder?.Invoke();
+    }
+}
