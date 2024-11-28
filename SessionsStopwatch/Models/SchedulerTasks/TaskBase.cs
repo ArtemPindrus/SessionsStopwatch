2e2ac12575aefcd5785ad44a3cee3631c3ae4691
Artem Pindrus <artempindrus@gmail.com>
2024-11-28 19:56:58 +0200

    Split registration and initialization of task

--- SessionsStopwatch/Models/SchedulerTasks/TaskBase.cs
+++ SessionsStopwatch/Models/SchedulerTasks/TaskBase.cs
@@ -2,6 +2,10 @@
 
 public abstract class TaskBase {
     public abstract string TaskName { get; }
-
-    public abstract void AddTask();
+    
+    public void RegisterTask() {
+        TaskService.Instance.RootFolder.RegisterTaskDefinition(TaskName, CreateTaskDefinition());
+    }
+    
+    protected abstract TaskDefinition CreateTaskDefinition();
 }
