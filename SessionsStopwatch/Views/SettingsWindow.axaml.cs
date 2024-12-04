34388deb97da59cd814f1d36095e50fb34e9bcb8
Artem Pindrus <artempindrus@gmail.com>
2024-12-04 18:38:00 +0200

    CheckDataTemplates

--- SessionsStopwatch/Views/SettingsWindow.axaml.cs
+++ SessionsStopwatch/Views/SettingsWindow.axaml.cs
@@ -4,8 +4,13 @@ using System.Linq;
 using System.Reflection;
 using Avalonia.Controls;
 using Avalonia.Input;
+using Avalonia.Markup.Xaml.Templates;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Enums;
 using SessionsStopwatch.Models.Reminding;
 using SessionsStopwatch.Utilities;
+using SessionsStopwatch.ViewModels.Reminders;
+using SessionsStopwatch.Views.Reminders;
 
 namespace SessionsStopwatch.Views;
 
@@ -14,10 +19,32 @@ public partial class SettingsWindow : Window {
         InitializeComponent();
         this.RegisterDragWindow(this);
 
+        Assembly executingAssembly = Assembly.GetExecutingAssembly();
+        Type[] types = executingAssembly.GetTypes();
+
+        InitializeReminderTypeSelector(types);
+        CheckDataTemplates(types);
+    }
+
+    private void CheckDataTemplates(Type[] assemblyTypes) {
+        Type baseType = typeof(AddReminderBaseVM);
+        
+        foreach (var type in assemblyTypes.Where(x => x.IsAssignableTo(baseType) && x != baseType)) {
+            // TODO: throw if not defined in XAML
+            if (DataTemplates.Any(x => ((DataTemplate)x).DataType == type)) {
+                
+            }
+            else {
+                var messageBox = MessageBoxManager.GetMessageBoxStandard("Error!", $"DataTemplate for {type} wasn't defined in {nameof(SettingsWindow)}.axaml!");
+                messageBox.ShowAsync();
+            }
+        }
+    }
+
+    private void InitializeReminderTypeSelector(Type[] assemblyTypes) {
         Type baseType = typeof(Reminder);
 
-        List<Type?> items = Assembly.GetExecutingAssembly()
-            .GetTypes()
+        List<Type?> items = assemblyTypes
             .Where(x => x != baseType && x.IsAssignableTo(typeof(Reminder)))
             .ToList<Type?>();
         items.Add(null);
